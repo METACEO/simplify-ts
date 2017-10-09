a50536ff6ae8e92d70593d04ddb3ea0e29239720
James Allen <james.allen@meta.ceo>
2017-10-08 22:34:51 -0500

    Ignoring the dist folder; not publising the src folder; including typings/declarations.

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var array_points_1 = require("./array-points");
-exports.SimplifyAP = array_points_1.SimplifyAP;
-var object_points_1 = require("./object-points");
-exports.Simplify = object_points_1.Simplify;
